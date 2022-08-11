d8be7c8aead3f0a4dcabfbce1d16b6c398bd1e78
usuariodegit <107440434+usuariodegit@users.noreply.github.com>
2022-08-10 21:24:09 -0400

    Inicio de aplicacion

---
+++ src/main/java/com/uninorte/ejerciciouno/EjercicioDos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uninorte.ejerciciouno;
+
+/**
+ *
+ * @author Jorge Espinola
+ */
+public class EjercicioDos {
+    public static void main(String[] args){
+        System.out.println("Jorge Espinola");
+        System.out.println("Km 30 - Minga Guazù");
+        System.out.println("0982581428");
+    }
+    
+}
---
+++ src/main/java/com/uninorte/ejerciciouno/HorarioDeClase.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uninorte.ejerciciouno;
+
+/**
+ *
+ * @author Jorge Espinola
+ */
+public class HorarioDeClase {
+    public static void main (String[] args){
+        System.out.println("Lunes\t\t\t\tMartes\t\t\tMiercoles\t\tJueves\t\tViernes");
+        System.out.println("=====\t\t\t\t=====\t\t\t=====\t\t\t=====\t\t=====");
+        System.out.println("Proyectoinformatico\tArquitecturadecomputadora\tProgramacion4\t\tAuditoria\tIngenieradesoftware");
+    }
+    
+}
