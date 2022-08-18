6c7e2361e170942d6233fa6ad3844ca4628c1efa
usuariodegit <107440434+usuariodegit@users.noreply.github.com>
2022-08-17 20:13:05 -0400

    Modificacion de clase

--- src/main/java/com/uninorte/ejerciciouno/HorarioDeClase.java
+++ src/main/java/com/uninorte/ejerciciouno/HorarioDeClase.java
@@ -13,6 +13,7 @@ public class HorarioDeClase {
         System.out.println("Lunes\t\t\t\tMartes\t\t\tMiercoles\t\tJueves\t\tViernes");
         System.out.println("=====\t\t\t\t=====\t\t\t=====\t\t\t=====\t\t=====");
         System.out.println("Proyectoinformatico\tArquitecturadecomputadora\tProgramacion4\t\tAuditoria\tIngenieradesoftware");
+        System.out.println("Horario de clase");
     }
     
 }
